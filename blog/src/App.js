82cd907788ff6dc6e5dbfc5c8c78b97f766ecece
DohweeKim <cloudkim2024@gmail.com>
2021-04-08 21:06:22 -0400

    React state practice

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,40 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [strings, string_modify] = useState(['Personal Info', 'Tech Blogs', 'CP']);  //a 에는 변수, b 는 데이터를 변경하는 함수 
+  //ES6 Destructuring //스테이트는 변경되면 HTML 이 자동 재랜더링 됩니당. 개꿀띠
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the Dohwee's Blog
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>def Leo_Dohwee_Kim(): </div>
+      </div>
+    
+      <div className="list">
+        <h3> { strings[0] } </h3>
+        <p> last updated : Apr 8th 2021 </p>
+        <hr/>
+      </div>
+
+      <div className="list">
+        <h3> { strings[1] } </h3>
+        <p> last updated : Apr 8th 2021 </p>
+        <hr/>
+      </div>
+
+      <div className="list">
+        <h3> { strings[2] } </h3>
+        <p> last updated : Apr 8th 2021 </p>
+        <hr/>
+      </div>
+
+      <div className="list">
+        <h5> "Software and cathedrals are much the same – first we build them, then we pray." </h5>
+        <p> Sam Redwine </p>
+      </div>
+
     </div>
   );
 }
