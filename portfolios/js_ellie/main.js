40db82e3f7a823f9169a8ac9442a01e916ba8570
Dohwee Kim <cloudkim2024@gmail.com>
2022-01-30 18:56:11 -0500

    Callback Function Named , Non Named

--- portfolios/js_ellie/main.js
+++ portfolios/js_ellie/main.js
@@ -1,3 +1,57 @@
 'use strict';
 
 console.log('hello world!');
+let name = 'elli';
+name = 'hi'
+console.log(name);
+
+let dk_obj = {
+    age : 28
+};
+
+let age = 15;
+
+console.log(`hi I am ${dk_obj.age} \n years old`);
+
+
+//default value
+function showMessage(message, from = 'unknown') {
+    console.log(`${message} arrived. from ${from}`);
+
+}
+
+//Rest parameters . Added in ES6
+function printAll(...args){
+    for (let i=0; i<args.length; i++) {
+        console.log(args[i]);
+    }
+
+    //or 
+
+    // in 을 쓰면 index 가 나감
+    for (const arg of args) {
+        console.log(arg);
+    }
+}
+printAll('hi','I','am','dk');
+
+//Callback 예제 
+function randomQuiz(answer, printYes, printNo){
+    if (answer === 'I love you') {
+        printYes();
+    }
+    else {
+        printNo();
+    }
+}
+
+const printYes = function () {
+    console.log('Yes!');
+}
+
+const printNo = function () {
+    console.log('No');
+}
+
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('I love you', printYes, printNo);
