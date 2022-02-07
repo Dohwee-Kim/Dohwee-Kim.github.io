f944efe9dd92a09f7e1cda9af351cc7eaa31a568
Leo Kim <cloudkim2024@gmail.com>
2022-02-06 20:46:34 -0500

    TODO app skeleton built with Node.js / Express lib

--- portfolios/Node_JS/server.js
+++ portfolios/Node_JS/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 //객체를 만들어 주세요 
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 //listen 2개 패러미터 ( , 포트 -> 띄운후 실행할 코드 )
 app.listen(8080, function() {
@@ -16,3 +18,22 @@ app.get('/pet', function(request, response) {
 app.get('/beauty', function(request, response) {
     response.send('now you are on beauty page!');
 });
+
+// 홈페이지로 접속했을때 
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
+
+// 홈페이지로 접속했을때 
+app.get('/write', function(req, res) {
+    res.sendFile(__dirname + '/write.html');
+})
+
+app.post('/add', function(request, response){
+    //request 갖다 쓸려면 또다른 라이브러리가 필요  : body-parser
+    response.send('Sent !!');
+    console.log(request.body.title);
+    console.log(request.body.date);
+
+    //now store to DB
+})
