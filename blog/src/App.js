a7d62e1785975c16d182f7c9051815361e796543
Leo Kim <cloudkim2024@gmail.com>
2022-04-25 10:23:47 -0400

    CSS study

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   let [strings, string_modify] = useState(['Personal Info', 'Projects portfolio', 'Certificates', 'About me']);  //a 에는 변수, b 는 데이터를 변경하는 함수 
-
+  
   //ES6 Destructuring //스테이트는 변경되면 HTML 이 자동 재랜더링 됩니당. 개꿀띠
   let [likes, likes_modify] = useState(0); // 위에 import 하는거 잊으면 안됨, likes_modify 는 변경용 함수, [state, state_modify_func] // re-render
 
